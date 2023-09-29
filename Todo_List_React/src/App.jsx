928bfe4e97bf7b6e03d7894915fd301e1a3dd514
Swastik <swastikpanda32@gmail.com>
2023-09-29 14:07:32 +0530

    Added Todo list logic to render and delete todos

--- Todo_List_React/src/App.jsx
+++ Todo_List_React/src/App.jsx
@@ -5,9 +5,39 @@ import './index.css';
 export default function App(){
   const [newItem, setnewItem] = useState("")
   const updateItem = (e) => setnewItem(e.target.value)
+  const [todos,setTodos] = useState([]);
+
+  const handleSubmit = (h) => {
+    h.preventDefault();
+    setTodos(currentTodos => {
+      return (
+        ([...currentTodos,{id:crypto.randomUUID(),title: newItem,completed:false}])
+      )
+    })
+    
+    setnewItem("");
+  } 
+
+  function toggleTodo(id,completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo,completed}
+        }
+
+        return todo;
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
   return (
     <>
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit}  className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
         <input value={newItem} onChange={updateItem} type="text" id="item" />
@@ -16,24 +46,20 @@ export default function App(){
     </form>
     <h1 className="header">Todo List</h1>
     <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox" />
-          Item 1
-        </label>
-        <button className='btn btn-danger'>
-           Delete
-        </button>
-      </li>
-      <li>
-        <label>
-          <input type="checkbox" />
-          Item 2
-        </label>
-        <button className='btn btn-danger'>
-           Delete
-        </button>
-      </li>
+      {todos.map(todo => {
+        return (
+          <li key={todo.id}>
+          <label>
+            <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id,e.target.checked)} />
+            {todo.title}
+          </label>
+          <button onClick={() => deleteTodo(todo.id)}  className='btn btn-danger'>
+             Delete
+          </button>
+        </li>
+        )
+      })}
+
     </ul>
     </>
   )
